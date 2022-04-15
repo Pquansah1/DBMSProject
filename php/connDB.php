dd557e843fbf6f3c33d1110a3d079f92772a6db8
Peregrino <pquansah110@gmail.com>
2022-04-15 13:27:41 -0400

    revamped

--- php/connDB.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db_name = "collegeevents";
-$mysql_username = "root";
-$mysql_password = "root";
-$server_name = "localhost";
-$conn = mysqli_connect($server_name, $mysql_username, $mysql_password, $db_name);
-?>
